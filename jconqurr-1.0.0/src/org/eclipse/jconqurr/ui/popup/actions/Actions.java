2e68c053a901fa77f4b11f502faff06b599fd514
snipergacp <snipergacp@35a5bf38-ba88-11de-ad03-79fce359684b>
2009-10-17 09:49:04 +0000

    Added the project building function Modified:plugin.xml,MANIFEST.MF,org.eclipse.jconqurr.ui.popup.actions.Actions.java

--- jconqurr-1.0.0/src/org/eclipse/jconqurr/ui/popup/actions/Actions.java
+++ jconqurr-1.0.0/src/org/eclipse/jconqurr/ui/popup/actions/Actions.java
@@ -1,16 +1,30 @@
 package org.eclipse.jconqurr.ui.popup.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.launching.IVMInstall;
+import org.eclipse.jdt.launching.JavaRuntime;
+import org.eclipse.jdt.launching.LibraryLocation;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 
 public class Actions implements IObjectActionDelegate {
 
-	private Shell shell;
-	
 	/**
 	 * Constructor for Action1.
 	 */
@@ -22,17 +36,65 @@ public class Actions implements IObjectActionDelegate {
 	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
 	 */
 	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
-		shell = targetPart.getSite().getShell();
+		 
 	}
 
 	/**
 	 * @see IActionDelegate#run(IAction)
 	 */
 	public void run(IAction action) {
-		MessageDialog.openInformation(
-			shell,
-			"Jconqurr user interface",
-			"Build Project was executed.");
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IProject project = root.getProject("NewProject");
+		IProject[] projects = root.getProjects();
+
+		for (IProject p : projects) {
+			if (!(p.getName().equals("NewProject"))) {
+				System.out.println("Working in project " + project.getName());
+			}
+		}
+
+		try {
+			project.create(null);
+			project.open(null);
+			IProjectDescription description = project.getDescription();
+			description.setNatureIds(new String[] { JavaCore.NATURE_ID });
+			project.setDescription(description, null);
+			IJavaProject javaProject = JavaCore.create(project);
+			IFolder binFolder = project.getFolder("bin");
+			binFolder.create(false, true, null);
+			List<IClasspathEntry> entries = new ArrayList<IClasspathEntry>();
+			IVMInstall vmInstall = JavaRuntime.getDefaultVMInstall();
+			LibraryLocation[] locations = JavaRuntime
+					.getLibraryLocations(vmInstall);
+
+			for (LibraryLocation element : locations) {
+				entries.add(JavaCore.newLibraryEntry(element
+						.getSystemLibraryPath(), null, null));
+			}
+			// add libs to project class path
+			javaProject.setRawClasspath(entries
+					.toArray(new IClasspathEntry[entries.size()]), null);
+			IFolder sourceFolder = project.getFolder("src");
+			sourceFolder.create(true, true, null);
+			IPackageFragmentRoot src = javaProject
+					.getPackageFragmentRoot(sourceFolder);
+			IClasspathEntry[] oldEntries = javaProject.getRawClasspath();
+			IClasspathEntry[] newEntries = new IClasspathEntry[oldEntries.length + 1];
+			System.arraycopy(oldEntries, 0, newEntries, 0, oldEntries.length);
+			newEntries[oldEntries.length] = JavaCore.newSourceEntry(src
+					.getPath());
+			javaProject.setRawClasspath(newEntries, javaProject
+					.getOutputLocation(), null);
+			IPackageFragment pack = src
+					.createPackageFragment("x.y", true, null);
+			String str = "package x.y;" + "\n" + "public class E{" + "\n"
+					+ "String first;" + "\n" + "}";
+			ICompilationUnit cu = pack.createCompilationUnit("E.java", str,
+					false, null);
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
