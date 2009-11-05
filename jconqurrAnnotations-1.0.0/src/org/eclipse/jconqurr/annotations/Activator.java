d02b94f9a1bf153c392edfcc6c2b65c2ec8452ad
lasantha.fdo <lasantha.fdo@35a5bf38-ba88-11de-ad03-79fce359684b>
2009-11-05 04:22:14 +0000

    Fixed spelling in name

--- jconqurrAnnotations-1.0.0/src/org/eclipse/jconqurr/annotations/Activator.java
+++ jconqurrAnnotations-1.0.0/src/org/eclipse/jconqurr/annotations/Activator.java
@@ -1,4 +1,4 @@
-package org.eclipse.jconqurr.annotaions;
+package org.eclipse.jconqurr.annotations;
 
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends Plugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.jconqurr.annotaions";
+	public static final String PLUGIN_ID = "org.eclipse.jconqurr.annotations";
 
 	// The shared instance
 	private static Activator plugin;
