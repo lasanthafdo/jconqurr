2be0c3380ec46cdd1d7b3bd98d329a16198bb3f5
lasantha.fdo <lasantha.fdo@35a5bf38-ba88-11de-ad03-79fce359684b>
2009-11-07 14:21:22 +0000

    Developed ForLoopModifier. Rewriting successful!

--- org.eclipse.jconqurr.core/src/org/eclipse/jconqurr/core/parallel/IForLoopModifier.java
+++ org.eclipse.jconqurr.core/src/org/eclipse/jconqurr/core/parallel/IForLoopModifier.java
@@ -2,13 +2,21 @@ package org.eclipse.jconqurr.core.parallel;
 
 import java.util.List;
 
+import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.ForStatement;
 
+/**
+ * 
+ * @author lasantha
+ *
+ */
 public interface IForLoopModifier {
 	public void analyzeCode();
+	public void modifyCode();
 	public String getModifiedCode();
 	public void setCompilationUnit(CompilationUnit cu);
 	public CompilationUnit getCompilationUnit();
+	public Block getModifiedBlock();
 	public List<ForStatement> getForStatements();
 }
