fb54913b008d0db2c5867c4ec6ff4bf9708adfc7
snipergacp <snipergacp@35a5bf38-ba88-11de-ad03-79fce359684b>
2010-03-07 18:35:10 +0000

    refactor the GPUHandler Code

--- branches/Jconqurr_v-0.0.1/org.eclipse.jconqurr.core/src/org/eclipse/jconqurr/core/gpu/GPUHandler.java
+++ branches/Jconqurr_v-0.0.1/org.eclipse.jconqurr.core/src/org/eclipse/jconqurr/core/gpu/GPUHandler.java
@@ -21,24 +21,27 @@ import org.eclipse.jdt.core.dom.MethodDeclaration;
 public class GPUHandler implements IGPUHandler {
 
 	private MethodDeclaration method;
-	private String cubinFileName = "JCudaCubinSample_kernel.cu";
-	private String cuMethodArguments;
+	
 	private String gloabalOutPut;
+	private String leftOperandOfBodyStatement;
+	private String rightOperandOfBodyStatement;
+	private String operatorOfBodyStatement;
 	private String noThreds;
 	private String updater;
 	private String leftOperandForCondition;
 	private String rightOperandForCondition;
 	private String operatorForCondition;
 	private String initializer;
+	private final String tidx = "tidx";
+	private final String cubinFileName = "simple.cu";
 	private List<String> globalInputs = new ArrayList<String>();
 
-	
 	private String getCubinFileDeclaration() {
 		String cubinDeclaraion = "String cubinFileName = prepareCubinFile(\""
 				+ cubinFileName + "\");";
 		return cubinDeclaraion;
 	}
-	 
+
 	@Override
 	public void process() {
 		System.out.println("Called the process.......................");
@@ -47,122 +50,249 @@ public class GPUHandler implements IGPUHandler {
 		this.method.accept(forLoopVisitor);
 		List<ForStatement> forLoops = forLoopVisitor.getForLoops();
 		for (ForStatement s : forLoops) {
-			List initialisers = s.initializers();
-			Expression expression = s.getExpression();
-			List updaters = s.updaters();
-			initializer="";
-			for (Object obj : initialisers) {
-				initializer=initializer+((Expression) obj).toString();
-				System.out.println("Initializers :"
-						+ ((Expression) obj).toString());
-			}
-			updater="";
-			for (Object obj : updaters) {
-				System.out
-						.println("Updaters: " + ((Expression) obj).toString());
-				updater=updater+((Expression) obj).toString();
-			}
-			System.out.println("Expression: " + expression.toString());
-			InfixExpressionVisitor infixVisitor = new InfixExpressionVisitor();
-			expression.accept(infixVisitor);
-			System.out
-					.println("Left operand:" + infixVisitor.getLeftHandSide());
-			System.out.println("Condition operand: "
-					+ infixVisitor.getOperator());
-			leftOperandForCondition=infixVisitor.getLeftHandSide();
-			operatorForCondition=infixVisitor.getOperator();
-			rightOperandForCondition=infixVisitor.getRightHandSide();
-			System.out.println("Right operand & no of threads: "
-					+ infixVisitor.getRightHandSide());
-			noThreds = infixVisitor.getRightHandSide();
-			 
-			System.out.println("Body:" + s.getBody());
-			s.getBody();
-			ExpressionStatementVisitor expVisitior = new ExpressionStatementVisitor();
-			s.getBody().accept(expVisitior);
-			for (ExpressionStatement ex : expVisitior.getExpressionStatements()) {
-				AssignmentVisitor asignVisitor = new AssignmentVisitor();
-				ex.accept(asignVisitor);
-				asignVisitor.getLeftHandSide();
-				System.out.println("Left hand Side: "
-						+ asignVisitor.getLeftHandSide());
-				if (asignVisitor.getLeftHandSide().getNodeType() == Assignment.ARRAY_ACCESS) {
-					System.out.println("array access");
-					ArrayAccess arrayAccess = (ArrayAccess) asignVisitor
-							.getLeftHandSide();
-					gloabalOutPut = getVariablePointer(asignVisitor
-							.getLeftHandSide().toString(), gloabalOutPut,
-							arrayAccess.resolveTypeBinding().getName());
-				}
-				System.out.println("Right hand side: "
-						+ asignVisitor.getRightHandSide());
-				infixVisitor.getInfixExpressions().clear();
-				asignVisitor.getRightHandSide().accept(infixVisitor);
-				infixVisitor.getLeftHandSide();
-				System.out.println(infixVisitor.getLeftHandSide());
+			handleLoopDeclaration(s);
+			handleLoopBody(s);
+			/*
+			 * List initialisers = s.initializers(); Expression expression =
+			 * s.getExpression(); List updaters = s.updaters(); initializer =
+			 * ""; for (Object obj : initialisers) { initializer = initializer +
+			 * ((Expression) obj).toString();
+			 * System.out.println("Initializers :" + ((Expression)
+			 * obj).toString()); } updater = ""; for (Object obj : updaters) {
+			 * System.out .println("Updaters: " + ((Expression)
+			 * obj).toString()); updater = updater + ((Expression)
+			 * obj).toString(); } InfixExpressionVisitor infixVisitor = new
+			 * InfixExpressionVisitor(); expression.accept(infixVisitor);
+			 * 
+			 * System.out.println("Expression: " + expression.toString());
+			 * System.out .println("Left operand:" +
+			 * infixVisitor.getLeftHandSide());
+			 * System.out.println("Condition operand: " +
+			 * infixVisitor.getOperator());
+			 * 
+			 * leftOperandForCondition = infixVisitor.getLeftHandSide();
+			 * operatorForCondition = infixVisitor.getOperator();
+			 * rightOperandForCondition = infixVisitor.getRightHandSide();
+			 * noThreds = infixVisitor.getRightHandSide();
+			 * 
+			 * System.out.println("Right operand & no of threads: " +
+			 * infixVisitor.getRightHandSide());
+			 */
+			// ///////////////////////////////////////////////////
 
-				infixVisitor.getRightHandSide();
-				System.out.println(infixVisitor.getRightHandSide());
-				for (InfixExpression in : infixVisitor.getInfixExpressions()) {
-					System.out.println(in.getLeftOperand());
-					if (in.getLeftOperand().getNodeType() == Assignment.ARRAY_ACCESS) {
-						in.getLeftOperand().resolveTypeBinding();
-						ArrayAccess arrayAccess = (ArrayAccess) in
-								.getLeftOperand();
-						SimpleNameVisitor simpleNameVisitor = new SimpleNameVisitor();
-						arrayAccess.accept(simpleNameVisitor);
-						String identifier = simpleNameVisitor.getIdentifier();
-						System.out.println(arrayAccess.resolveTypeBinding()
-								.getName()
-								+ "-------------");
-
-						globalInputs.add(getVariablePointer(in.getLeftOperand()
-								.toString(), identifier, arrayAccess
-								.resolveTypeBinding().getName()));
-						arrayAccess.getArray().accept(simpleNameVisitor);
-
-					}
-					if (in.getRightOperand().getNodeType() == Assignment.ARRAY_ACCESS) {
-						in.getLeftOperand().resolveTypeBinding();
-						ArrayAccess arrayAccess = (ArrayAccess) in
-								.getRightOperand();
-						SimpleNameVisitor simpleNameVisitor = new SimpleNameVisitor();
-						arrayAccess.accept(simpleNameVisitor);
-						System.out.println(arrayAccess.resolveTypeBinding()
-								.toString());
-						String identifier = simpleNameVisitor.getIdentifier();
-						System.out.println(arrayAccess.resolveTypeBinding()
-								.getName()
-								+ "-------------");
-
-						globalInputs.add(getVariablePointer(in
-								.getRightOperand().toString(), identifier,
-								arrayAccess.resolveTypeBinding().getName()));
-						arrayAccess.getArray().accept(simpleNameVisitor);
-
-					}
-					System.out.println(in.getOperator());
-					System.out.println(in.getRightOperand());
-					System.out.println("gloabal inputs.......................");
-					for (String g : globalInputs) {
-						System.out.println(g);
-					}
-				}
-				asignVisitor.getRightHandSide();
-			}
+			/*
+			 * InfixExpressionVisitor infixVisitor = new
+			 * InfixExpressionVisitor();
+			 * 
+			 * ExpressionStatementVisitor expVisitior = new
+			 * ExpressionStatementVisitor(); s.getBody().accept(expVisitior);
+			 * 
+			 * for (ExpressionStatement ex :
+			 * expVisitior.getExpressionStatements()) { AssignmentVisitor
+			 * asignVisitor = new AssignmentVisitor(); ex.accept(asignVisitor);
+			 * asignVisitor.getLeftHandSide();
+			 * System.out.println("Left hand Side: " +
+			 * asignVisitor.getLeftHandSide()); operatorOfBodyStatement =
+			 * asignVisitor.getOperator().toString(); if
+			 * (asignVisitor.getLeftHandSide().getNodeType() ==
+			 * Assignment.ARRAY_ACCESS) { System.out.println("array access");
+			 * ArrayAccess arrayAccess = (ArrayAccess) asignVisitor
+			 * .getLeftHandSide(); gloabalOutPut =
+			 * getVariablePointer(asignVisitor .getLeftHandSide().toString(),
+			 * gloabalOutPut, arrayAccess.resolveTypeBinding().getName());
+			 * leftOperandOfBodyStatement = getModifiedArray(asignVisitor
+			 * .getLeftHandSide().toString()); }
+			 * System.out.println("Right hand side: " +
+			 * asignVisitor.getRightHandSide());
+			 * infixVisitor.getInfixExpressions().clear();
+			 * asignVisitor.getRightHandSide().accept(infixVisitor);
+			 * infixVisitor.getLeftHandSide();
+			 * System.out.println(infixVisitor.getLeftHandSide());
+			 * 
+			 * infixVisitor.getRightHandSide();
+			 * System.out.println(infixVisitor.getRightHandSide()); for
+			 * (InfixExpression in : infixVisitor.getInfixExpressions()) {
+			 * System.out.println(in.getLeftOperand()); if
+			 * (in.getLeftOperand().getNodeType() == Assignment.ARRAY_ACCESS) {
+			 * in.getLeftOperand().resolveTypeBinding();
+			 * rightOperandOfBodyStatement = getModifiedArray(in
+			 * .getLeftOperand().toString()); ArrayAccess arrayAccess =
+			 * (ArrayAccess) in .getLeftOperand(); SimpleNameVisitor
+			 * simpleNameVisitor = new SimpleNameVisitor();
+			 * arrayAccess.accept(simpleNameVisitor); String identifier =
+			 * simpleNameVisitor.getIdentifier();
+			 * System.out.println(arrayAccess.resolveTypeBinding() .getName() +
+			 * "-------------");
+			 * 
+			 * globalInputs.add(getVariablePointer(in.getLeftOperand()
+			 * .toString(), identifier, arrayAccess
+			 * .resolveTypeBinding().getName()));
+			 * arrayAccess.getArray().accept(simpleNameVisitor);
+			 * 
+			 * } rightOperandOfBodyStatement = rightOperandOfBodyStatement +
+			 * in.getOperator(); if (in.getRightOperand().getNodeType() ==
+			 * Assignment.ARRAY_ACCESS) {
+			 * in.getLeftOperand().resolveTypeBinding(); ArrayAccess arrayAccess
+			 * = (ArrayAccess) in .getRightOperand(); SimpleNameVisitor
+			 * simpleNameVisitor = new SimpleNameVisitor();
+			 * arrayAccess.accept(simpleNameVisitor);
+			 * System.out.println(arrayAccess.resolveTypeBinding() .toString());
+			 * String identifier = simpleNameVisitor.getIdentifier();
+			 * System.out.println(arrayAccess.resolveTypeBinding() .getName() +
+			 * "-------------"); rightOperandOfBodyStatement =
+			 * rightOperandOfBodyStatement +
+			 * getModifiedArray(in.getRightOperand() .toString());
+			 * globalInputs.add(getVariablePointer(in
+			 * .getRightOperand().toString(), identifier,
+			 * arrayAccess.resolveTypeBinding().getName()));
+			 * arrayAccess.getArray().accept(simpleNameVisitor);
+			 * 
+			 * } System.out.println(in.getOperator());
+			 * System.out.println(in.getRightOperand());
+			 * System.out.println("gloabal inputs......................."); for
+			 * (String g : globalInputs) { System.out.println(g); } }
+			 * asignVisitor.getRightHandSide(); }
+			 */
 			writeToCUFile(s);
+			// System.out.println(getModifiedCode());
 		}
 		System.out.println("end the process.......................");
 	}
 
+	private void handleLoopBody(ForStatement s) {
+		InfixExpressionVisitor infixVisitor = new InfixExpressionVisitor();
+		ExpressionStatementVisitor expVisitior = new ExpressionStatementVisitor();
+		s.getBody().accept(expVisitior);
+		for (ExpressionStatement ex : expVisitior.getExpressionStatements()) {
+			AssignmentVisitor asignVisitor = new AssignmentVisitor();
+			ex.accept(asignVisitor);
+			asignVisitor.getLeftHandSide();
+			System.out.println("Left hand Side: "
+					+ asignVisitor.getLeftHandSide());
+			operatorOfBodyStatement = asignVisitor.getOperator().toString();
+			if (asignVisitor.getLeftHandSide().getNodeType() == Assignment.ARRAY_ACCESS) {
+				System.out.println("array access");
+				ArrayAccess arrayAccess = (ArrayAccess) asignVisitor
+						.getLeftHandSide();
+				gloabalOutPut = getVariablePointer(asignVisitor
+						.getLeftHandSide().toString(), gloabalOutPut,
+						arrayAccess.resolveTypeBinding().getName());
+				leftOperandOfBodyStatement = getModifiedArray(asignVisitor
+						.getLeftHandSide().toString());
+			}
+			System.out.println("Right hand side: "
+					+ asignVisitor.getRightHandSide());
+			infixVisitor.getInfixExpressions().clear();
+			asignVisitor.getRightHandSide().accept(infixVisitor);
+			infixVisitor.getLeftHandSide();
+			System.out.println(infixVisitor.getLeftHandSide());
+			infixVisitor.getRightHandSide();
+			System.out.println(infixVisitor.getRightHandSide());
+			for (InfixExpression in : infixVisitor.getInfixExpressions()) {
+				System.out.println(in.getLeftOperand());
+				if (in.getLeftOperand().getNodeType() == Assignment.ARRAY_ACCESS) {
+					in.getLeftOperand().resolveTypeBinding();
+					rightOperandOfBodyStatement = getModifiedArray(in
+							.getLeftOperand().toString());
+					ArrayAccess arrayAccess = (ArrayAccess) in.getLeftOperand();
+					SimpleNameVisitor simpleNameVisitor = new SimpleNameVisitor();
+					arrayAccess.accept(simpleNameVisitor);
+					String identifier = simpleNameVisitor.getIdentifier();
+					System.out.println(arrayAccess.resolveTypeBinding()
+							.getName()
+							+ "-------------");
+					globalInputs.add(getVariablePointer(in.getLeftOperand()
+							.toString(), identifier, arrayAccess
+							.resolveTypeBinding().getName()));
+					arrayAccess.getArray().accept(simpleNameVisitor);
+				}
+				rightOperandOfBodyStatement = rightOperandOfBodyStatement
+						+ in.getOperator();
+				if (in.getRightOperand().getNodeType() == Assignment.ARRAY_ACCESS) {
+					in.getLeftOperand().resolveTypeBinding();
+					ArrayAccess arrayAccess = (ArrayAccess) in
+							.getRightOperand();
+					SimpleNameVisitor simpleNameVisitor = new SimpleNameVisitor();
+					arrayAccess.accept(simpleNameVisitor);
+					System.out.println(arrayAccess.resolveTypeBinding()
+							.toString());
+					String identifier = simpleNameVisitor.getIdentifier();
+					System.out.println(arrayAccess.resolveTypeBinding()
+							.getName()
+							+ "-------------");
+					rightOperandOfBodyStatement = rightOperandOfBodyStatement
+							+ getModifiedArray(in.getRightOperand().toString());
+					globalInputs.add(getVariablePointer(in.getRightOperand()
+							.toString(), identifier, arrayAccess
+							.resolveTypeBinding().getName()));
+					arrayAccess.getArray().accept(simpleNameVisitor);
+				}
+				System.out.println(in.getOperator());
+				System.out.println(in.getRightOperand());
+				System.out.println("gloabal inputs.......................");
+				for (String g : globalInputs) {
+					System.out.println(g);
+				}
+			}
+		}
+	}
+
+	private void handleLoopDeclaration(ForStatement s) {
+		List<?> initialisers = s.initializers();
+		Expression expression = s.getExpression();
+		List<?> updaters = s.updaters();
+		initializer = "";
+		for (Object obj : initialisers) {
+			initializer = initializer + ((Expression) obj).toString();
+			System.out
+					.println("Initializers :" + ((Expression) obj).toString());
+		}
+		updater = "";
+		for (Object obj : updaters) {
+			System.out.println("Updaters: " + ((Expression) obj).toString());
+			updater = updater + ((Expression) obj).toString();
+		}
+		InfixExpressionVisitor infixVisitor = new InfixExpressionVisitor();
+		expression.accept(infixVisitor);
+
+		System.out.println("Expression: " + expression.toString());
+		System.out.println("Left operand:" + infixVisitor.getLeftHandSide());
+		System.out.println("Condition operand: " + infixVisitor.getOperator());
+
+		leftOperandForCondition = infixVisitor.getLeftHandSide();
+		operatorForCondition = infixVisitor.getOperator();
+		rightOperandForCondition = infixVisitor.getRightHandSide();
+		noThreds = infixVisitor.getRightHandSide();
+
+		System.out.println("Right operand & no of threads: "
+				+ infixVisitor.getRightHandSide());
+	}
+
+	private String getModifiedArray(String exp) {
+		int count = 0;
+		String identifier = "";
+		for (int l = 0; l < exp.length(); l++) {
+			if (exp.charAt(l) == '[') {
+				if (count > 0) {
+					count++;
+				} else {
+					identifier = exp.substring(0, l);
+					count++;
+				}
+			}
+		}
+		if (count == 1) {
+			return identifier + "[" + tidx + "]";
+		} else
+			return "";
+	}
+
 	private String getVariablePointer(String exp, String name, String type) {
 		int count = 0;
-		int lenght = exp.length();
-		String leftHandSide = exp;
 		String pointerVariable = "";
 		String identifier = "";
-		for (int l = 0; l < lenght; l++) {
-			if (leftHandSide.charAt(l) == '[') {
+		for (int l = 0; l < exp.length(); l++) {
+			if (exp.charAt(l) == '[') {
 				if (count > 0) {
 					count++;
 				} else {
@@ -177,7 +307,6 @@ public class GPUHandler implements IGPUHandler {
 				pointer = pointer + "*";
 			}
 			pointerVariable = type + pointer + " " + identifier;
-			System.out.println(pointerVariable);
 		}
 		return pointerVariable;
 	}
@@ -202,7 +331,7 @@ public class GPUHandler implements IGPUHandler {
 			System.err.println("Unable to write to file: " + e.getMessage());
 		}
 	}
-	
+
 	private String getCuDeclaration() {
 		String decl = "extern \"C\"\n" + "__global__ void sampleKernel(";
 		String inputArg = "";
@@ -217,12 +346,14 @@ public class GPUHandler implements IGPUHandler {
 	private String getCuFileBody() {
 		String variabels = "const unsigned int tidX = threadIdx.x;" + "\n"
 				+ "globalOutputData[tidX] = 0;" + "\n";
-		String body = "C[tidX]=A[tidx]+B[tidx];\n";
-		String sync = "__syncthreads();";
-		return "{" + variabels + body + sync + "}";
+		String body = leftOperandOfBodyStatement + operatorOfBodyStatement
+				+ rightOperandOfBodyStatement + ";\n";
+		String sync = "__syncthreads();\n";
+		return "{\n" + variabels + body + sync + "}";
 	}
- 
-	public String getModifiedCode(){
-		return JCUDABinding.getTobytearraycode()+JCUDABinding.getGetpreparecubinfilecode();
+
+	public String getModifiedCode() {
+		return JCUDABinding.getTobytearraycode()
+				+ JCUDABinding.getGetpreparecubinfilecode();
 	}
 }
