f8e5ccc1b42b3802f0cf10e1d95eaa89e95a3eba
snipergacp <snipergacp@35a5bf38-ba88-11de-ad03-79fce359684b>
2010-03-07 05:12:56 +0000

    Added the infixExpression Visitor and modified the ExpressionStatement Visitor and the Assignement Visitor

--- org.eclipse.jconqurr.core/src/org/eclipse/jconqurr/core/ast/visitors/AssignmentVisitor.java
+++ org.eclipse.jconqurr.core/src/org/eclipse/jconqurr/core/ast/visitors/AssignmentVisitor.java
@@ -3,15 +3,31 @@ package org.eclipse.jconqurr.core.ast.visitors;
 
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.Assignment;
+import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.IBinding;
 import org.eclipse.jdt.core.dom.SimpleName;
 
 public class AssignmentVisitor extends ASTVisitor {
-	
+	private Expression leftHandSide;
+	/**
+	 * @return the leftHandSide
+	 */
+	public Expression getLeftHandSide() {
+		return leftHandSide;
+	}
+	/**
+	 * @return the rightHandSide
+	 */
+	public Expression getRightHandSide() {
+		return rightHandSide;
+	}
+	private Expression rightHandSide;
 	public boolean visit(Assignment node) {
+		leftHandSide=node.getLeftHandSide();
+		rightHandSide=node.getRightHandSide();
 		if (node.getLeftHandSide() instanceof SimpleName) {
-			System.out.println(node.toString());
-			System.out.println(node.getLeftHandSide());
+			//System.out.println(node.toString());
+			//System.out.println(node.getLeftHandSide());
 			/*IBinding binding = ((SimpleName) node.getLeftHandSide())
 					.resolveBinding();
 			if (localVariableManagers.containsKey(binding)) {
