ebc9e23aaf2476cf559890d580a49dbfa998ec73
snipergacp <snipergacp@35a5bf38-ba88-11de-ad03-79fce359684b>
2010-06-06 11:01:05 +0000

    Added New Directives to Pipeline process

--- org.eclipse.jconqurr.directives/src/org/eclipse/jconqurr/directives/Directives.java
+++ org.eclipse.jconqurr.directives/src/org/eclipse/jconqurr/directives/Directives.java
@@ -1,6 +1,17 @@
 package org.eclipse.jconqurr.directives;
 
-public class Directives {
-public static final void task(){};
-
+public final class Directives {
+public static final void startTask(){};
+public static final void endTask(){};
+public static final void forLoop(){};
+public static final void barrier(){};
+public static final void pipelineInput(){};
+public static final void pipelineInput(String input,String output){};
+public static final void pipelineStage(){};
+public static final void pipelineStage(String input,String output){};
+public static final void pipelineOutput(){};
+public static final void pipelineStart(){};
+public static final void pipelineEnd(){};
+public static final void shared(String shared){};
 }
+ 
