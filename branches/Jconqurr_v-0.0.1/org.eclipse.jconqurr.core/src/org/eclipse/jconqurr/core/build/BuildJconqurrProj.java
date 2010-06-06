c94ec3c59cfb96dd1aa61d29b178c34442ad4bee
snipergacp <snipergacp@35a5bf38-ba88-11de-ad03-79fce359684b>
2010-06-06 10:55:31 +0000

    Reformat the BuildJconqurrProject

--- branches/Jconqurr_v-0.0.1/org.eclipse.jconqurr.core/src/org/eclipse/jconqurr/core/build/BuildJconqurrProj.java
+++ branches/Jconqurr_v-0.0.1/org.eclipse.jconqurr.core/src/org/eclipse/jconqurr/core/build/BuildJconqurrProj.java
@@ -1,26 +1,43 @@
 package org.eclipse.jconqurr.core.build;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.launching.JavaRuntime;
 
 public class BuildJconqurrProj implements IBuildJconqurrProjManager {
 
-	 /**
-	  * @see IBuildJconqurrProjManager#createProject(IJavaProject)
-	  */
+	/**
+	 * @see IBuildJconqurrProjManager#createProject(IJavaProject)
+	 */
 	public IJavaProject createProject(IJavaProject selectedProject) {
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
 		IProject project = root.getProject("Jq"
 				+ selectedProject.getElementName());
+		List<IPath>libraryPath=new ArrayList<IPath>();
+		try {
+			IPackageFragmentRoot[] roots = selectedProject
+					.getPackageFragmentRoots();
+			for (IPackageFragmentRoot r : roots) {
+				if(!(r.getElementName().equals("src"))){
+					libraryPath.add(r.getPath());
+				}
+			}
+		} catch (JavaModelException e2) {
+			e2.printStackTrace();
+		}
 		if (!project.exists()) {
 			try {
 				// Create a project
@@ -31,12 +48,19 @@ public class BuildJconqurrProj implements IBuildJconqurrProjManager {
 				description.setNatureIds(new String[] { JavaCore.NATURE_ID });
 				project.setDescription(description, null);
 				// set the build path
-				 
 				IJavaProject javaProject = JavaCore.create(project);
 				IClasspathEntry[] buildPath = {
 						JavaCore.newSourceEntry(project.getFullPath().append(
 								"src")),
 						JavaRuntime.getDefaultJREContainerEntry() };
+				IClasspathEntry[] libPaths=new IClasspathEntry[libraryPath.size()+1];
+				for(int i=0;i<libraryPath.size();i++){
+					libPaths[i+1]=JavaCore.newLibraryEntry((libraryPath.get(i)),
+							null, null, true);
+				}
+				libPaths[0]=JavaCore.newSourceEntry(project.getFullPath()
+						.append("src"));
+				buildPath=libPaths;
 				javaProject.setRawClasspath(buildPath, project.getFullPath()
 						.append("bin"), null);
 				// create source folder
@@ -45,11 +69,9 @@ public class BuildJconqurrProj implements IBuildJconqurrProjManager {
 				IPackageFragmentRoot srcFolder = javaProject
 						.getPackageFragmentRoot(folder);
 			} catch (CoreException e) {
-				System.out.println("Project exist");
 				try {
 					return (IJavaProject) project.getNature(JavaCore.NATURE_ID);
 				} catch (CoreException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
@@ -57,10 +79,9 @@ public class BuildJconqurrProj implements IBuildJconqurrProjManager {
 		try {
 			return (IJavaProject) project.getNature(JavaCore.NATURE_ID);
 		} catch (CoreException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 			return null;
 		}
 	}
- 
+
 }
