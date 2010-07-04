2fa75223ed7765a9d70cce17d59dfc3a15fb8def
lasantha.fdo <lasantha.fdo@35a5bf38-ba88-11de-ad03-79fce359684b>
2010-07-04 13:25:40 +0000

    Branching annotation library to dev_experimental

---
+++ org.eclipse.jconqurr.annotations/src/org/eclipse/jconqurr/annotations/processors/GPUProcessor.java
@@ -0,0 +1,15 @@
+package org.eclipse.jconqurr.annotations.processors;
+
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.TypeElement;
+
+public class GPUProcessor extends AbstractProcessor {
+	public boolean process(Set<? extends TypeElement> arg0,
+			RoundEnvironment arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
